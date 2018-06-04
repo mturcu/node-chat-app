686d7f9f7d59aa868d3d1ff5dbb4d692a2de3b13
Mihai Turcu <mturcu@gmail.com>
2018-06-04 09:48:03 +0300

    Update

---
+++ config/config.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const path = require('path');
+
+const env = process.env.NODE_ENV || 'development';
+if (env === 'development' || env === 'test') {
+  const jsonConfig = require('./config.json');
+  const envConfig = jsonConfig[env];
+  Object.keys(envConfig).forEach(key => process.env[key] = envConfig[key]);
+}
+
+module.exports = {
+  authHeader: 'x-auth',
+  access: 'auth',
+  publicPath: path.join(__dirname, '../public'),
+  secret: process.env.JWT_SECRET,
+  port: process.env.PORT,
+  mongoUrl: process.env.MONGODB_URI
+}
